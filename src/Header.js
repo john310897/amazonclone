e4c2203b77f733b97cff51df7b98a47a68a92f5c
john <john310897@gmail.com>
2021-03-04 23:53:32 +0530

    homepage updated
    homepage

---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Header.css';
+import { Search } from '@material-ui/icons';
+const Header = props => {
+    return (
+        <div className='header_section'>
+             <img src='https://onlinebusinessmanager.com/wp-content/uploads/2018/09/white-amazon-logo-png-6.png'/>
+             <div className='search'>
+                 <input type='text' className='search'/>
+                {/* logo */}
+                <Search className='search_icon'/>
+             </div>
+             <div className='header_nav'>
+                  <div className='option'>
+                    <span>hello guest</span>
+                    <span>sign in</span>
+                  </div>
+                  <div className='option'>
+                    <span>returns</span>
+                  </div>
+                  <div className='option'>
+                      <span>cart</span>
+                  </div>
+             </div>
+         </div>
+    ); 
+};
+
+export default Header;
